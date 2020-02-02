0017631aaf0ddf304ca3eb34c7c0ede945373e3c
Baozhu Zuo <zuobaozhu@gmail.com>
2020-02-02 12:32:03 +0800

    Pretty printed the Arduino code with astyle

--- examples/FadeInOut/FadeInOut.ino
+++ examples/FadeInOut/FadeInOut.ino
@@ -1,8 +1,8 @@
 
-/* 
- * Example of using the ChainableRGB library for controlling a Grove RGB.
- * This code fades in an out colors in a strip of leds.
- */
+/*
+    Example of using the ChainableRGB library for controlling a Grove RGB.
+    This code fades in an out colors in a strip of leds.
+*/
 
 
 #include <ChainableLED.h>
@@ -11,23 +11,21 @@
 
 ChainableLED leds(7, 8, NUM_LEDS);
 
-void setup()
-{
+void setup() {
 }
 
 byte power = 0;
 
-void loop()
-{
-  for (byte i=0; i<NUM_LEDS; i++)
-  {
-    if (i%2 == 0)
-      leds.setColorRGB(i, power, 0, 0);
-    else
-      leds.setColorRGB(i, 0, 255-power, 0);
-  }  
-  power+= 10;
-  
-  delay(10);
+void loop() {
+    for (byte i = 0; i < NUM_LEDS; i++) {
+        if (i % 2 == 0) {
+            leds.setColorRGB(i, power, 0, 0);
+        } else {
+            leds.setColorRGB(i, 0, 255 - power, 0);
+        }
+    }
+    power += 10;
+
+    delay(10);
 }
 
--- examples/SimpleCycle/SimpleCycle.ino
+++ examples/SimpleCycle/SimpleCycle.ino
@@ -1,8 +1,8 @@
 
-/* 
- * Example of using the ChainableRGB library for controlling a Grove RGB.
- * This code puts a red dot (led) moving along a strip of blue dots.
- */
+/*
+    Example of using the ChainableRGB library for controlling a Grove RGB.
+    This code puts a red dot (led) moving along a strip of blue dots.
+*/
 
 #include <ChainableLED.h>
 
@@ -10,23 +10,21 @@
 
 ChainableLED leds(7, 8, NUM_LEDS);
 
-void setup()
-{
+void setup() {
 }
 
 byte pos = 0;
 
-void loop()
-{
-  for (byte i=0; i<NUM_LEDS; i++)
-  {
-    if (i==pos)
-      leds.setColorRGB(i, 255, 0, 0);  
-    else
-      leds.setColorRGB(i, 0, 0, 255); 
-  }
-  delay(250);
-  
-  pos = (pos+1) % NUM_LEDS;
+void loop() {
+    for (byte i = 0; i < NUM_LEDS; i++) {
+        if (i == pos) {
+            leds.setColorRGB(i, 255, 0, 0);
+        } else {
+            leds.setColorRGB(i, 0, 0, 255);
+        }
+    }
+    delay(250);
+
+    pos = (pos + 1) % NUM_LEDS;
 }
 
